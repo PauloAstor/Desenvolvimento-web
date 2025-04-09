d8d0e046326a1e4f4f925497d956d2aabd7eb5ca
PauloAstor <pauloaugusttoz1@gmail.com>
2025-04-09 20:56:46 -0300

    Add files via upload

---
+++ index (2).js
@@ -0,0 +1,11 @@
+function calcular(num1, num2, operacao) {
+  let resultado;
+
+  switch (operacao) {
+    case '+':
+      resultado = num1 + num2;
+      console.log(`Resultado: ${num1} + ${num2} = ${resultado}`);
+      break;
+    
+  }
+}
